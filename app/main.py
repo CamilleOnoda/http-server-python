74fabe26befe91bb8bd591da785ef1eda4d95df9
Camille Onoda <info@camilleonoda.com>
2025-09-26 17:03:43 +0900

    feat: server responds to an HTTP request with a 200 response

--- app/main.py
+++ app/main.py
@@ -7,11 +7,15 @@ def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     server_socket.bind(("localhost", 4221))
-    server_socket.listen()
+    server_socket.listen(1)
 
     while True:
         conn, address = server_socket.accept()
-        conn.close()
+        try:
+            data = conn.recv(1024)
+            conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        finally:
+            conn.close()
 
 if __name__ == "__main__":
     main()
