31cbd2d888594b6407b4c525711247a077e8466b
Camille Onoda <info@camilleonoda.com>
2025-10-02 16:56:07 +0900

    codecrafters submit [skip ci]

--- app/http.py
+++ app/http.py
@@ -2,10 +2,10 @@ from codecs import decode
 from .constants import CRLF
 
 
-def extract_url_path(request_target):
-    decoded_request = request_target.decode('utf-8', errors='replace')
-    request_line = decoded_request.split(CRLF)[0]
-    parts = request_line.split(" ")
+def extract_url_path(raw_request):
+    text = raw_request.decode('utf-8', errors='replace')
+    request_line = text.split(CRLF, 1)[0]
+    parts = request_line.split(" ", 2)
     if len(parts) != 3:
         raise ValueError("Malformed request line."
                          "Example: 'GET /index.html HTTP/1.1\r\n'")
