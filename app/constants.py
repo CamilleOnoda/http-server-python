033f34a95a63112ca9b744e446f4bdab4cfecc6c
Camille Onoda <info@camilleonoda.com>
2025-10-02 15:36:44 +0900

    fix: keep CRLF and END_HEADERS as a string

--- app/constants.py
+++ app/constants.py
@@ -4,5 +4,5 @@ PORT = 4221
 HTTP_CODE_200 = "HTTP/1.1 200 OK\r\n\r\n"
 HTTP_CODE_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
 
-CRLF = b"\r\n"
-END_HEADERS = b"\r\n\r\n"
+CRLF = "\r\n"
+END_HEADERS = "\r\n\r\n"
