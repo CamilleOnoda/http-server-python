6506a4354307bf0f944d897a934d62381eb888a0
Camille Onoda <info@camilleonoda.com>
2025-10-02 15:46:39 +0900

    fix: add the Server class parameters

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from .server import Server
-
+from .constants import HOST, PORT
 
 def main():
     print("Logs from my program will appear here!")
 
-    Server().server_forever()
+    Server(HOST,PORT).server_forever()
 
 if __name__ == "__main__":
     main()
