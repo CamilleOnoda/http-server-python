939e44ab6ce058df055d863b469500c4a1cef05e
Camille Onoda <info@camilleonoda.com>
2025-10-06 16:53:36 +0900

    test: add TestResponseBody class covering /echo, root, and invalid path

--- tests/test_network_service.py
+++ tests/test_network_service.py
@@ -1,6 +1,6 @@
 import unittest
 import socket
-from app.http import extract_url_path
+from app.http import extract_url_path, handle_request
 
 
 class TestNetworkService(unittest.TestCase):
@@ -30,11 +30,36 @@ class TestExtractPath(unittest.TestCase):
         url_path = extract_url_path(request.encode())
         self.assertEqual(url_path, '/')
 
-    def test_invalid(self):
+    def test_no_path(self):
         request = "GET HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n"
         with self.assertRaises(ValueError):
             extract_url_path(request.encode())
         
+
+class TestResponseBody(unittest.TestCase):
+    def test_echo(self):
+        request = "GET /echo/abc HTTP/1.1\r\nHost: localhost:4221\r\n"
+        url_path = extract_url_path(request.encode())
+        body = handle_request(url_path)
+        self.assertEqual(body, 
+                         b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n"
+                         b"Content-Length: 3\r\n\r\nabc")
+    
+    def test_invalid_path(self):
+        request = "GET /raspberry HTTP/1.1\r\nHost: localhost:4221\r\n\r\n"
+        url_path = extract_url_path(request.encode())
+        body = handle_request(url_path)
+        self.assertEqual(body, 
+                         b"HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n"
+                         b"Content-Length: 0\r\n\r\n")
+        
+    def test_no_content(self):
+        request = "GET / HTTP/1.1\r\nHost: localhost:4221\r\n\r\n"
+        url_path = extract_url_path(request.encode())
+        body = handle_request(url_path)
+        self.assertEqual(body, 
+                         b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n"
+                         b"Content-Length: 0\r\n\r\n")
 
 
 if __name__ == "__main__":
