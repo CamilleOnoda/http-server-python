7260dbbaa6c4c04369c91b58dd72b3988738ad55
Camille Onoda <info@camilleonoda.com>
2025-10-06 15:53:40 +0900

    chore: add constants.py for shared constants

--- app/constants.py
+++ app/constants.py
@@ -1,8 +1,8 @@
 HOST = "localhost"
 PORT = 4221
 
-HTTP_CODE_200 = "HTTP/1.1 200 OK\r\n\r\n"
-HTTP_CODE_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
+HTTP_CODE_200 = "HTTP/1.1 200 OK"
+HTTP_CODE_404 = "HTTP/1.1 404 Not Found"
 
 CRLF = "\r\n"
 END_HEADERS = "\r\n\r\n"
