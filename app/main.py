3612f0962a84f3ecbc63ca38eac82333d064c18b
Camille Onoda <info@camilleonoda.com>
2025-10-16 15:00:51 +0900

    chore: remove the print statement

--- app/main.py
+++ app/main.py
@@ -2,8 +2,6 @@ from .server import Server
 from .constants import HOST, PORT
 
 def main():
-    print("Logs from my program will appear here!")
-
     Server(HOST,PORT).server_forever()
 
 if __name__ == "__main__":
