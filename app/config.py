df0f3ea5b1827097559c514c585336a9da577107
Camille Onoda <info@camilleonoda.com>
2025-10-31 15:33:30 +0900

    feat: add server configuration defaults

--- app/config.py
+++ app/config.py
@@ -1,5 +1,13 @@
 class ServerConfig: 
-    def __init__(self, host, port, directory): 
+    def __init__(self,
+                 host,
+                 port,
+                 root_dir,
+                 ): 
         self.host = host 
         self.port = port 
-        self.directory = directory
+        self.root_dir = root_dir
+        self.max_headers_bytes = 32_768
+        self.max_body_bytes = 10_000_000
+        self.max_concurrent_connections = 32
+        self.recv_time_out = 5.0
